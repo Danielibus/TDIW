2ed19037e63d6fb7ce136c98163ff7d61611dfbd
Dani <65300433+Danielibus@users.noreply.github.com>
2022-11-01 11:41:53 +0100

    m

--- Index.php
+++ Index.php
@@ -4,8 +4,9 @@ $accio = $_GET['accio'] ?? NULL;
 switch ($accio) {
     case 'llistar-categories':
     include __DIR__.'/resource_llistar_categories.php';
-    break; default:
-    include __DIR__.'/resource_portada.php';
-    break; }
+    break; 
+    default:
+        include __DIR__.'/resource_portada.php';
+        break; }
 
 ?>  
