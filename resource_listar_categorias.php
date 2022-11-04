bdf9ca2b85aa3f22451336904ef17b3c23cae713
Dani <65300433+Danielibus@users.noreply.github.com>
2022-11-04 11:46:43 +0100

    m

--- resource_listar_categorias.php
+++ resource_listar_categorias.php
@@ -1,8 +1,12 @@
-
+<html lang="es">
+    <head>
+        <title>Tu tienda de moda online</title>
+    </head>
     <header>
-        <?php require_once __DIR__ . '/Controladores/menu_superior.php'; ?>
+        <?php require_once __DIR__ . '/controladores/menu_superior.php'; ?>
     </header>
 
     <div class ="container">
-    <?php require_once __DIR__ . '/Controladores/listar_categorias.php'; ?>
+    <?php require_once __DIR__ . '/controladores/listar_categorias.php'; ?>
     </div>  
+</html>
